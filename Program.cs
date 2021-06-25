ea36e1978768eb631e8934d1481cb01318ae1e8a
Atiq Rahman <atiqcx@gmail.com>
2021-06-25 13:16:48 -0700

    Initial Statiq Project - add Entry Point Program for static - add VSCode project file - settings file - enable Search Index Generation

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+namespace iQubitNote
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .DeployToGitHubPagesBranch("atiq-cs", "note", Config.FromSetting<string>("GITHUB_TOKEN"), "web")
+                .RunAsync();
+    }
+}
